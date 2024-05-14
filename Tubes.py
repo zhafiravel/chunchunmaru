f685a58cc1adb988f6e53e72e72445eacf3f563f
setya31 <79081950+setya31@users.noreply.github.com>
2024-05-14 19:48:52 +0700

    Update Tubes.py

--- Tubes.py
+++ Tubes.py
@@ -2,3 +2,18 @@
 daftar_belanjaan = []
 daftar_harga = {}
 
+# Fungsi untuk menambahkan item ke daftar belanjaan
+# Putra Strata Tandika Setyawan (2311104050)
+def tambah_item(items):
+    new_items = items.split(",")  # Pisahkan item yang dimasukkan dengan koma
+    for item in new_items:
+        item = item.strip().capitalize()  # Hilangkan spasi di awal dan akhir item dan kapitalisasi huruf pertama
+        if item:
+            harga = input_ulang(f"Masukkan harga untuk {item}: ")
+            while not harga.isdigit():
+                print("Harga harus berupa angka. Silakan coba lagi.")
+                harga = input_ulang(f"Masukkan harga untuk {item}: ")
+            daftar_belanjaan.append(item)
+            daftar_harga[item] = int(harga)
+            print(f"{item} telah ditambahkan ke daftar belanjaan dengan harga {harga}.")
+    sortir_belanjaan()
