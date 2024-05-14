34495c6193b066c5d551699f0e75a6f5c72d2bdb
rabanialya <alyarabani33@gmail.com>
2024-05-14 20:53:58 +0800

    Update Tubes.py

--- Tubes.py
+++ Tubes.py
@@ -26,3 +26,14 @@ def cari_item(item):
         print(f"{item} ada dalam daftar belanjaan dengan harga {daftar_harga[item]}.")
     else:
         print(f"{item} tidak ada dalam daftar belanjaan.")
+
+# Fungsi untuk menghapus item dari daftar belanjaan
+# Alya Rabani (2311104076)
+def hapus_item(item):
+    item = item.capitalize()  # Kapitalisasi huruf pertama
+    if item in daftar_belanjaan:
+        daftar_belanjaan.remove(item)
+        del daftar_harga[item]
+        print(f"{item} telah dihapus dari daftar belanjaan.")
+    else:
+        print(f"{item} tidak ada dalam daftar belanjaan.")
